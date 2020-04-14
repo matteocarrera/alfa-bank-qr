3728a7c9ed1774fa3a7bfa3e8ee9220f89dfd503
Vladimir <matteocarrera@mail.ru>
2020-04-14 19:07:55 +0500

    Реализован экран загрузки (проверить на телефоне)

--- app/src/main/java/com/example/alpha_bank_qr/SplashActivity.kt
+++ app/src/main/java/com/example/alpha_bank_qr/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.example.alpha_bank_qr
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT : Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
