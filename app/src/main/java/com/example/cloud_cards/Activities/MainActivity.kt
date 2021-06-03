4bb1bac93cc35b643be15848b5df1726305da0b7
Макаров Владимир Игоревич <vmakarov3@alfabank.ru>
2021-06-03 19:45:41 +0500

    feature(): link import added

--- app/src/main/java/com/example/cloud_cards/Activities/MainActivity.kt
+++ app/src/main/java/com/example/cloud_cards/Activities/MainActivity.kt
@@ -9,14 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.cloud_cards.Database.AppDatabase
 import com.example.cloud_cards.R
 import com.example.cloud_cards.Utils.QRUtils
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var db : AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +24,20 @@ class MainActivity : AppCompatActivity() {
         setupWithNavController(navView, navController)
         navView.setupWithNavController(navController)
 
-        db = AppDatabase.getInstance(applicationContext)
-
         // Получение QR-визитки в виде изображения вне приложения
         if (intent.action == Intent.ACTION_SEND && intent.type?.startsWith("image/") == true)
             handleSendImage(intent)
         else if (intent.action == Intent.ACTION_SEND_MULTIPLE && intent.type?.startsWith("image/") == true)
             handleSendMultipleImages(intent)
+
+        handleBusinessCardLink()
+    }
+
+    private fun handleBusinessCardLink() {
+        val uri = intent.data
+        if (uri != null) {
+            QRUtils.getUserFromQR(uri.toString(), applicationContext)
+        }
     }
 
     // Обработка полученного изображения вне приложения
